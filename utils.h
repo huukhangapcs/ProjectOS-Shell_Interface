8f5912b114701651ce26fc1244d84cd65277c657
Nguyen Khang <44218750+huukhangapcs@users.noreply.github.com>
2021-05-11 18:39:48 +0700

    Update utils.h

--- utils.h
+++ utils.h
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <dirent.h>
+#include<stdlib.h>
 #include <string.h>
 void getHistory(){
     FILE *fp;
@@ -41,5 +42,57 @@ void listSubDirectory(){
     printf("\n");
     closedir(dir);
 }
+void splitToken(char* cmd, char* args[]){
+    char *token = strtok(cmd, " ");
+    int i = 0;
+    args[i] = token;
+    while(token != NULL){
+    	args[i++] = token;
+    	token = strtok(NULL, " ");
+    }
+}
+int cmpr(const void *a, const void *b) { 
+   return (strcmp(*(char **)a, *(char **)b));
+}
+void sort_a(void *array, unsigned n) { 
+   qsort(array, n, sizeof(const char *), cmpr);
+}
+void sortFile(char* fileName){
+    char *lines[1000];  //limit 1000 lines
+    char line[1024]; // 1 line 1024 characters
+    int cnt = 0;
+    FILE *file = fopen(fileName, "r");
+
+    if (file != NULL)
+    {
+        while (fgets(line, sizeof (line), file) != NULL)
+        {
+            line[strcspn(line, "\n")] = '\0';
+            if (cnt < sizeof (lines)/ sizeof (*lines))
+            {
+                lines[cnt++] = strdup(line);
+            }
+            else
+            {
+                break;
+            }
+        }
+        fclose(file);
+        sort_a(lines, cnt);
+        for (int j = 0; j < cnt; j++)
+        {
+            printf("%s\n", lines[j]);
+        }
+        for (int j = 0; j < cnt; j++)
+        {
+            free(lines[j]);
+        }
+    }
+    else
+    {
+        perror(fileName);
+    }
+}
+
 
 #endif
