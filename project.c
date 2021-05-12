81a5790b8d6911ac5166176d18af6c3308dc96d8
Nguyen Khang <44218750+huukhangapcs@users.noreply.github.com>
2021-05-12 15:15:22 +0700

    Update project.c

--- project.c
+++ project.c
@@ -3,6 +3,44 @@
 #include <string.h>
 #include "utils.h"
 #define MAX_LINE 80 /* The maximum length command */
+int iDirection = 0;
+int oDirection = 0;
+char *iDirectionFile="\0";
+char* oDirectionFile = "\0";
+
+void resetState(char *args[]){
+    int iDirection = 0;
+    int oDirection = 0;
+    char *iDirectionFile="\0";
+    char* oDirectionFile = "\0";
+    for(int i = 0; i <(MAX_LINE/2 + 1);i++)
+		args[i] = '\0';
+}
+void splitToken(char* cmd, char* args[]){
+    resetState(args);
+    char *token = strtok(cmd, " ");
+    int i = 0;
+    args[i] = token;
+    while(token != NULL){
+        if (!strcmp(token,">")){
+            oDirection = 1;
+            args[i++] = token;
+    	    token = strtok(NULL, " ");
+            oDirectionFile = token;
+        } else if (!strcmp(token,"<")){
+            iDirection = 1;
+            args[i++] = token;
+    	    token = strtok(NULL, " ");
+            iDirectionFile = token;
+        }
+        else{
+            args[i++] = token;
+    	    token = strtok(NULL, " ");
+        }
+    	
+    }
+}
+
 
 int main(void)
 {
@@ -17,52 +55,70 @@ int main(void)
             while( fgets(final_history, sizeof(final_history), fp)!=NULL ){
                 // loop to get the final line of history file
             }
-            // if (final_history[0] != '\0')
-            //     printf("1 : %s\n", final_history);
-
-
             while (run) {
-                
                         printf("osh>");
                         fflush(stdout);
-                        for(int i = 0; i <(MAX_LINE/2 + 1);i++)
-				            args[i] = '\0';
                         fgets(cmd, MAX_LINE, stdin);
                         if (strcmp(cmd, final_history) != 0)
-                            fputs(cmd, fp);
+                            fputs(cmd, fp); // save to history file
                         strtok(cmd,"\n");
-                        if (strcmp(cmd, "history")==0){
+
+                        if (strcmp(cmd,"!!")==0){
+                            if (strcmp(final_history,"\0")==0){
+                                printf("No command in history\n");
+                                continue;
+                            }
+                            else{
+                                printf("Recently entered command: %s\n",final_history);
+                                strcpy(cmd, final_history);
+                            }
+                        }
+                        
+                        splitToken(cmd, args);
+                        if (strcmp(args[0], "exit")==0)
+                            break;
+                        //handleIR OR
+                        int stdin = 0;
+                        int stdout = 0;// use for IO REDIRECTION       
+                        if (iDirection)
+                            stdin = handleIR(iDirectionFile);
+                        else if (oDirection){ // start output REDIRECTION
+                            stdout = handleOR(oDirectionFile);
+                        }
+                            
+
+
+                        if (strcmp(args[0], "history")==0){
                             getHistory();
                         }
-                        else if (strcmp(cmd, "exit")==0)
-                            run = 0;
-                        else if (strcmp(cmd, "pwd")==0)
-                            getWorkingDirectory();
-                        else if (strcmp(cmd, "ls")==0)
-                            listSubDirectory();
-                        else{
-                            
-                            splitToken(cmd, args); 
-                            if (strcmp(args[0], "cd")==0){
-                                if (args[1] != NULL){
-                                    if (chdir(args[1])!=0){
-                                        perror(args[1]);
-                                    }
-                                }
-                            }
-                            else if (strcmp(args[0], "sort")==0){
-                                if (args[1] != NULL){
-                                    sortFile(args[1]);
-                                }
 
+                        else if (strcmp(args[0], "pwd")==0)
+                            getWorkingDirectory();
+                        else if (strcmp(args[0], "ls")==0)
+                            listSubDirectory();
+                        else if (strcmp(args[0], "cd")==0){
+                            if (args[1] != NULL){
+                                if (chdir(args[1])!=0){
+                                    perror(args[1]);
+                                }
                             }
                         }
+                        else if (strcmp(args[0], "sort")==0){
+                            if (args[1] != NULL){
+                                sortFile(args[1],args[2]);
+                            }
                         /**
                         * After reading user input, the steps are:
                         * (1) fork a child process using fork()
                         * (2) the child process will invoke execvp()
                         * (3) parent will invoke wait() unless command included &
                         */
+                       if (iDirection)
+                            stopOR(stdin); //stop Output redirection
+                       else if (oDirection)
+                            stopIR(stdout); // stop Input redirection
+                       strcpy(final_history, cmd);
+                        }
 
             }
             fclose(fp);
