dd8ae53c3a039d9beecc85097f35aa15077d4e7b
Nguyen Khang <44218750+huukhangapcs@users.noreply.github.com>
2021-05-14 21:45:09 +0700

    Update io_redirection.h

--- io_redirection.h
+++ io_redirection.h
@@ -1,11 +1,25 @@
 #ifndef __IOREDIRECTION___
 #define __IOREDIRECTION___
-
-void set_io_mode(char* args[], int iFlag, int oFlag, int *in, int *out){
-    
+#include <stdio.h>
+#include <unistd.h>
+#include<stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+void set_o_mode(char* args[], int oFlag, int* stdmode){
+    if (oFlag){
+        int out = open(args[oFlag], O_WRONLY | O_TRUNC | O_CREAT, S_IRUSR | S_IRGRP | S_IWGRP | S_IWUSR);
+        if (-1 == out) { perror(args[oFlag]); return; }
+        int save_out = dup(fileno(stdout));
+        if (-1 == dup2(out, fileno(stdout))) { perror("cannot redirect stdout"); return; }
+        *stdmode = save_out;
+    }
 }
-void reset_io_mode(char* args[], int iFlag, int oFlag, int *in, int *out){
-
+void reset_o_mode(char* args[], int oFlag, int *std){
+    if (oFlag && *std){
+        dup2(*std, fileno(stdout));
+        close(*std);
+    }
+    
 }
 
 #endif
